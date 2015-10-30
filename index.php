b4bb9bc4831fb9e34a2b8764631c045a69c00b42
RMMusic <r.m.alien.ua@gmail.com>
2015-10-30 14:18:24 +0200

    and now checkbox! :)

--- index.php
+++ index.php
@@ -1,35 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
+ * 20 Created by PhpStorm.
  * User: admin
  * Date: 22.10.2015
  * Time: 9:33
  */
 ?>
 <?php include'header.php'?>
-
-<div class="container1">
+    <form action="class/inOutDB.php" method="post">
     <table>
-        <?php for ($x = 0; $x <= 9; $x++):
+        <?php for ($x = 1; $x <= 10; $x++):
             echo '<tr>';
-            for($y = 0; $y <= 9; $y++):?>
-                <td class="item" data-coordinates="<?php echo $x ?>,<?php echo $y ?>"></td>
+            for($y = 1; $y <= 10; $y++):?>
+                <td>
+                    <input type="checkbox" class="checkShip" name="<?php echo $x ?>,<?php echo $y ?>">
+                </td>
             <?php endfor ?>
             </tr>
             <?php endfor ?>
     </table>
-
-    <div class="ship shipOne" data-wight="1"></div>
-    <div class="ship shipOne" data-wight="1"></div>
-    <div class="ship shipOne" data-wight="1"></div>
-    <div class="ship shipOne" data-wight="1"></div>
-    <div class="ship shipDouble shipDoubleHorizontal" data-position="horizontal" data-wight="2"></div>
-    <div class="ship shipDouble shipDoubleHorizontal" data-position="horizontal" data-wight="2"></div>
-    <div class="ship shipDouble shipDoubleHorizontal" data-position="horizontal" data-wight="2"></div>
-    <div class="ship shipTriple shipTripleHorizontal" data-position="horizontal" data-wight="3"></div>
-    <div class="ship shipTriple shipTripleHorizontal" data-position="horizontal" data-wight="3"></div>
-    <div class="ship shipFour shipFourHorizontal" data-position="horizontal" data-wight="4"></div>
-</div>
-
-
+        <button type="submit">!!!</button>
+    </form>
 <?php include'footer.php'?>
