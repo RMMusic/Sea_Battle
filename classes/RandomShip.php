abc152ce02a8c745b94a0c06b5f011b24e5d236e
demoffon <dmitroch7@gmail.com>
2015-11-19 16:30:30 +0200

    Generate 1Ship

--- classes/RandomShip.php
+++ classes/RandomShip.php
@@ -37,27 +37,51 @@ class RandomShip
         $shipArray=$this->ziroTabel();
         $kil=1;
         while($kil<=(5-$n)) {
-            $x = rand(1, 10);
-            $y = rand(1, 10);
+            $z = rand(1, 2);
+            if ($z==1) {
+                $x = rand(1, (11-$n));
+                $y = rand(1, 10);
 
-            if ($shipArray[$x][$y] == 0) {
+                    if ($shipArray[$x][$y] == 0 and $shipArray[$x+$n-1][$y] == 0) {
 
-                for ($i = -1; $i <= 1; $i++) {
-                    for ($j = -1; $j <= 1; $j++) {
-                        $shipArray[$x+$i][$y+$j] = 9;
+                        for ($i = -1; $i <= $n; $i++) {
+                            for ($j = -1; $j <= 1; $j++) {
+                                $shipArray[$x + $i][$y + $j] = 9;
+                            }
+                        }
+                        for($s=1;$s<=$n;$s++) {
+                            $shipArray[$x+$s-1][$y] = 1;
+                        }
+                        $kil++;
                     }
                 }
-                $shipArray[$x][$y] = 1;
-                $kil++;
+
+            else{
+                $y = rand(1, (11-$n));
+                $x = rand(1, 10);
+
+                if ($shipArray[$x][$y] == 0 and $shipArray[$x][$y+$n-1] == 0) {
+
+                    for ($i = -1; $i <= 1; $i++) {
+                        for ($j = -1; $j <= $n; $j++) {
+                            $shipArray[$x + $i][$y + $j] = 9;
+                        }
+                    }
+                    for($s=1;$s<=$n;$s++) {
+                        $shipArray[$x][$y+$s-1] = 1;
+                    }
+                    $kil++;
+                }
+                }
             }
 
-        }
         return $shipArray;
     }
 
 }
 $oneShip = new RandomShip();
-$p = $oneShip->oneShipArray(1);
+//$p1 = $oneShip->oneShipArray(1);
+$p2 = $oneShip->oneShipArray(3);
 echo '<table border="1">';
 for($x  = 1; $x <=10; $x++){
     echo '<tr>';
@@ -65,7 +89,8 @@ for($x  = 1; $x <=10; $x++){
         echo '<td width="20px"; height="20px";>';
 
 //        if (in_array($x.','.$y, $explodeShipsCoordinates)){
-        echo $p[$x][$y];
+//        echo $p1[$x][$y];
+        echo $p2[$x][$y];
 //        }
         echo '</td>';
     }
